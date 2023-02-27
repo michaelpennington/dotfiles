0a2dd899c60d0f1e3f39451233ea4439a2d77b38
Michael Pennington <michaeldanielpennington@gmail.com>
2023-02-27 12:50:19 -0500

    prepend instead of append to path in fish config

--- global/user/fish/.config/fish/config.fish
+++ global/user/fish/.config/fish/config.fish
@@ -22,8 +22,8 @@ if status is-interactive
     zoxide init fish | source
     starship init fish | source
     bass source /etc/profile
-    fish_add_path -P -a ~/.local/bin
-    fish_add_path -P -a ~/.cargo/bin
+    fish_add_path -P ~/.local/bin
+    fish_add_path -P ~/.cargo/bin
     if set -q SSH_CLIENT; or set -q SSH_CONNECTION; or set -q WSL_DISTRO_NAME; or set -q WSLENV; or set -q WSL_INTEROP
         bass source /usr/bin/wayland_disablement.sh
     else
